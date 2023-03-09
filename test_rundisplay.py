a6ba385bd29d5394a48a3a8ad02f2d063a563d7b
dwight9339 <david@david-white.dev>
2023-03-09 08:07:23 -0700

    Removed unecessary files and added .gitignore

--- test_rundisplay.py
+++
@@ -1,9 +0,0 @@
-from rundisplay import SignController
-
-ctlr = SignController()
-ctlr.scrollRL("Test 1")
-ctlr.reset()
-ctlr.flyInTop("Test 2", color=(200, 255, 80))
-ctlr.reset()
-ctlr.flyInBottom("Test 3", 5, (233, 124, 79))
-ctlr.reset()
